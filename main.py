dfc6399ed5dc85d75727c2277a1fe7b6927f5d19
bershakvasya <76277340+bershakvasya@users.noreply.github.com>
2022-02-21 21:04:36 +0300

    qwerty

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+import uvicorn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+counters = {}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get('/')
+def main():
+    return "7632"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+uvicorn.run(app)
